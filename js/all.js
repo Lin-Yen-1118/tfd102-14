c5847bc54aded0fe1e1cbde4fab18db1d9b6d513
Lin-Yen-1118 <goatruby@gmail.com>
2021-05-25 08:38:05 +0800

    RWD_footer

---
+++ js/all.js
@@ -0,0 +1,17 @@
+/*var hamberger_bar = document.getElementsByClassName("burger_menu")[0];
+console.log(hamberger_bar);
+hamberger_bar.addEventListener("click", function(){
+    // console.log("123");
+
+  });
+*/
+  
+
+$(".burger_menu").click(function(){
+    $(".header_bar").hide();
+    });
+    $(".burger_menu").click(function(){
+    $(".header_bar").show();
+    });
+  });
+  
