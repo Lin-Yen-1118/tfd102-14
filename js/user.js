7ed2981a47976fa03f52188f5e32fa59624a22d4
Lin-Yen-1118 <goatruby@gmail.com>
2021-06-02 22:27:30 +0800

    add newContent

---
+++ js/user.js
@@ -0,0 +1,7 @@
+var user_btn = document.getElementsByClassName("login_btn");
+user_btn.addEventListener("click", function () {
+  console.log(user_btn);
+});
+
+// var user_signin = document.getElementsByClassName("signin_btn");
+// function(user_btn)
